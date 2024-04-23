1673247301293d0b44983dbe7a1d18a0f769b868
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-04-23 18:41:03 +0500

    Time: 50 ms (72.51%), Space: 16.6 MB (16.67%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,18 +1,35 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0: 
-            return False
-
-        p1 = x
-        counter = 0 
-
-        while x != 0:
-            counter = (counter * 10) + (x % 10)
-            x = x // 10
-        return p1 == counter 
+        if x < 0 :
+            return False 
+        ''' 
+        because -121 will evaluate to 121- so no need to 
+        check for negative values 
         '''
-        counter % 10 gives us the last element
-        counter * 10 gives us the place i.e. 10^ 0 , 10^1 , 10^2 etc
-        x = x// 10 gives us the remaining digits in the original no 
-        and discards the last number 
+        original_no = x
+        counter = 0
         '''
+        doing this because we are going to be modifying the 
+        original no "x"
+        '''
+        while x > 0:
+            '''
+            we are going to keep dividign the no until
+            we get zero 
+            '''
+            counter = (counter * 10) + ( x % 10)
+            '''
+            counter * 10 gives us the location of the decimal 
+            part i.e. 10 ^0 , 10^1 , 10^ 2
+            etc
+
+            x % 10 gives us the last no from our input integer
+
+            '''
+            x = x // 10 
+            '''
+            storing the remaining integers from teh x value 
+            i.e. the remaining integer without the last
+            value as we have included that in teh "counter
+            '''
+        return original_no == counter
