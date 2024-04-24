547abfc26896b9f3da8c01ba4e4bf715826fb90e
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-04-24 06:31:54 +0500

    Time: 57 ms (42.13%) | Memory: 16.5 MB (96.87%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,4 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-       str_x = str(x)
-       return str_x == str_x[::-1]
+       string_x = str(x)
+       # typecasting the input from integer data type 
+       # to string data - type
+
+       return string_x == string_x[::-1]
+       # [::-1] operator reverses the string
