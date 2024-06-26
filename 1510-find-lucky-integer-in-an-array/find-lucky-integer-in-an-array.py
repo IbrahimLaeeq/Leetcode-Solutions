77138a28f62a4d81c61e259a6dcd9b5f229a54e6
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-06-26 20:09:59 +0500

    Time: 64 ms (23.95%) | Memory: 16.8 MB (12.60%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -1,13 +1,15 @@
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
-        ''' 
-        we can use a hash map here
-        '''
-        res = [-1]
-        Set = set(arr)
-
-        for i in Set:
-            if i == arr.count(i):
-                res.append(i)
-        
-        return max(res)
+        hash_map = {}
+        for a in arr:
+            if a not in hash_map:
+                hash_map[a] = 1
+            else:
+                hash_map[a] +=1
+        max_key = 0
+        for key, val in hash_map.items():
+            if key == val and key > max_key:
+                max_key = key
+        if max_key == 0:
+            return -1
+        return max_key
