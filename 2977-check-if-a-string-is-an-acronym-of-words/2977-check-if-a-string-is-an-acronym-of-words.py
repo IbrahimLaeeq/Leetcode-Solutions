2172b220333ed60db1f9beee6e28bf2ab88ccc0c
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-07-04 20:19:12 +0500

    Time: 56 ms (41.06%), Space: 16.5 MB (32.41%) - LeetHub

---
+++ 2977-check-if-a-string-is-an-acronym-of-words/2977-check-if-a-string-is-an-acronym-of-words.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        if len(words) != len(s):
+            return False
+        for i in range (len(words)):
+            if not words[i].startswith(s[i]):
+                return False
+        return True
+                
+
+            #how are we going to get the first char from every indice?
+
+        
