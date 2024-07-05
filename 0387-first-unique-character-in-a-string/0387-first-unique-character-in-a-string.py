351e9a9e1ebdabc4fb08350078d949206129397d
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-07-05 14:53:21 +0500

    Time: 130 ms (19.12%), Space: 17.1 MB (47.28%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,26 +1,31 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         '''
-        store the counts in a hash map on teh first 
-        loop and then once we have the hash map 
-        run another loop on the input and see that
-        for this specific character if its count in 
-        the map is equal to one
+        maybe in a map we have order so we create a map and then 
+        iterate or we culd create the map iteravitely no that would
+        not work bc we might haea "v" at teh second indice and then 
+        we would also have one at the last indice
+
+        crerate a map by looping and hten in the second iteratoin 
+        we would iterate throught the map and the first char 
+        whcih has a count i.e value of oned woudl be returned
+
+        we woudl get its indice by maybe passing it to the original string a
+        and then using. getindeix or something
+        no need to do this just iteate thorght hte string again 
+        and see the counta gainst a speicif cindice and then we could return taht 
+        first ele whose count is one
+
         '''
-        '''
-        1. we use a defaultdict bc we are mapping from 
-        char to int so here we could just define that
-        we are mapping to an int to avoid the key errors 
-        when the key doesnt exist before in the hasp map
-        i.e we encounter an ele for the first
-        time in the string
-        '''
-        hash_map = defaultdict(int)
-        # char -> int
-        for i in s:
-            hash_map[i] +=1
+
+        # creating a map
+        map = {}
+        for i in range (len(s)):
+            map[s[i]] = 1 + map.get(s[i], 0)
         
         for idx , no in enumerate(s):
-            if hash_map[no] == 1:
+            if map[no] == 1:
                 return idx
         return -1
+
+        
