85f7a560aa1aa6ec8bb8c33ec559b3bf1066ebca
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-07-11 09:54:40 +0500

    Time: 2036 ms (7.02%) | Memory: 20.5 MB (82.53%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,22 +1,21 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        '''
-        getting a no which sint a duplicate and then
-        looping to get a pair which isnt a duplicate
-        '''
         res = set()
-        nums.sort()
-        for idx , no in enumerate(nums):
-            l ,r = idx + 1 , len(nums) - 1
-            while l < r:
+        nums.sort() # O(nlogn)
+        for idx , no in enumerate(nums): #O(n)
+            l = idx + 1
+            r = len(nums) - 1
+            while l < r:# O(n)
                 Ts = no + nums[l] + nums[r]
-                if Ts>0:
-                    r-=1
-                elif Ts<0:
+                if Ts < 0:
                     l+=1
+                elif Ts >0:
+                    r-=1
                 else:
-                    res.add((no, nums[l] , nums[r] ))
+                    res.add((no  , nums[l] , nums[r]))
                     l+=1
         return res
-
-        
+        '''
+        time : O(nlogn) + O(n^2) = O(n^2)
+        space : O(N)
+        '''
