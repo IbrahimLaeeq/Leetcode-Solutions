b91c198bd4ed0daac76b554ee2426eaa378f86f2
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-07-11 08:52:55 +0500

    Time: 1916 ms (8.26%), Space: 20.4 MB (85.87%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,12 +4,9 @@ class Solution:
         getting a no which sint a duplicate and then
         looping to get a pair which isnt a duplicate
         '''
-        res = []
+        res = set()
         nums.sort()
         for idx , no in enumerate(nums):
-            if idx > 0 and no == nums[idx-1]:
-                continue
-            
             l ,r = idx + 1 , len(nums) - 1
             while l < r:
                 Ts = no + nums[l] + nums[r]
@@ -18,10 +15,8 @@ class Solution:
                 elif Ts<0:
                     l+=1
                 else:
-                    res.append([no, nums[l] , nums[r] ])
+                    res.add((no, nums[l] , nums[r] ))
                     l+=1
-                    while nums[l] == nums[l-1] and l < r:
-                        l+=1
         return res
 
         
