7013dd7fde268c9fa9a985c42222761af979222d
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-07-12 08:56:49 +0500

    Time: 93 ms (43.02%), Space: 19.9 MB (61.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,22 +3,9 @@ class Solution:
         map = {}
         res = []
         for s in strs:
-            srt = str(sorted(s))
-            if srt not in map:
-                map[srt] = []
-            map[srt].append(s)
-        return list(map.values())
-        '''
-        class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        #anagram strings are after sorting the same 
-        mp ={}
-        
-        for s in strs:
-            f=str(sorted(s))
-            if f not in mp:
-                mp[f]=[]
-            mp[f].append(s)
-        return list(mp.values())
-        '''
+            f = str(sorted(s))
+            if f not in map:
+                map[f] = []
+            map[f].append(s)
+        return list(map.values())        
         
