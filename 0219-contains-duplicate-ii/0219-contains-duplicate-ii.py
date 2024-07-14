b267af1c7b7b39aecfe40c647805504a546a392d
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-07-14 11:14:50 +0500

    Time: 468 ms (33.1%), Space: 28 MB (88.23%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        l = 0
         window = set()
-        l = 0 
         for r in range(len(nums)):
             if r - l > k:
                 window.remove(nums[l])
@@ -10,7 +10,10 @@ class Solution:
                 return True
             window.add(nums[r])
         return False
-
-''' 
-we basically iterate and add the values as long as our 
         '''
+        Time : O(N)
+        Space: O(K)
+        '''
+
+
+        
