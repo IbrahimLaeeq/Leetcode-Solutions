041596c10bf0d384f2b014b9f22edab0edd8ccf9
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-07-22 07:06:40 +0500

    Time: 172 ms (50.3%), Space: 18 MB (57.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        size = len(nums)
-        return nums[size//2]
+        #boyer moore algo
+        res,count = 0,0
+        for i in nums:
+            if count == 0:
+                res = i
+            if i == res:
+                count+=1
+            else:
+                count-=1
+        return res
