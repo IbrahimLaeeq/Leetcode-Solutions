c4a656e4b77f993a8e81f2421c6f9d6fe80e184e
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-07-22 07:11:22 +0500

    Time: 182 ms (16.18%) | Memory: 17.9 MB (57.28%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,12 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        #boyer moore algo
-        res,count = 0,0
-        for i in nums:
-            if count == 0:
-                res = i
-            if i == res:
-                count+=1
-            else:
-                count-=1
-        return res
+        size = len(nums)
+        hash_map = {}
+
+        for i in range(len(nums)):
+            hash_map[nums[i]] = 1 + hash_map.get(nums[i],0)
+        
+        n = size // 2
+
+        for key,val in hash_map.items():
+            if val > n:
+                return key
+        
+        
