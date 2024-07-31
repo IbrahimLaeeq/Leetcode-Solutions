6beeacb8a293e37d5729ea53d7c8125146cef800
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-07-31 09:50:04 +0500

    Time: 72 ms (55.58%) | Memory: 17 MB (73.21%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,26 +1,26 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        #kandanes algorithm 
+        #forward pass
+        curr_prod = 1
         max_prod = float('-inf')
-        curr_prod = 1
-
-        for i in range(len(nums)):
-            curr_prod *= nums[i] 
-            max_prod = max(curr_prod, max_prod)
+        for i in nums:
+            curr_prod = i * curr_prod
+            max_prod = max(max_prod, curr_prod)
             if curr_prod == 0:
                 curr_prod = 1
-            
-            # resetting the prodcut if it becomes one
-
+        #reverse pass
         curr_prod = 1
-        # resetting again for the reverse pass
-
-        for i in range(len(nums) - 1 , -1 , -1 ): # start , end , jump
-        # by default goes from left to right so we have to specify
-            curr_prod *= nums[i] 
-            max_prod = max(curr_prod, max_prod)
+        for i in range(len(nums)- 1, - 1 , -1) :  # (start, end, jump)
+            curr_prod = nums[i] * curr_prod
+            max_prod = max(max_prod, curr_prod)
             if curr_prod == 0:
                 curr_prod = 1
-            # resetting the prodcut if it becomes one
         
         return max_prod
+
+        
+
+
+
         
