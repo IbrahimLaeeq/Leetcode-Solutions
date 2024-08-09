ea330f9916980665afb503ac3037400acae485b7
Muhammad Ibrahim Laeeq <133008492+IbrahimLaeeq@users.noreply.github.com>
2024-08-09 08:33:58 +0500

    Time: 41 ms (23.08%), Space: 16.5 MB (66.5%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -9,14 +9,24 @@ class Solution:
         if not root:
             return []
         queue = collections.deque([root])
+        # intializing queue with the root for the while loop
         ans = [root.val]
+        # the first ele will be in the answer regardless
         while queue:
             for i in range(len(queue)):
-                node = queue.popleft()
+                node=queue.popleft()
+                #pop the first ele from the queue
                 if node.left:
                     queue.append(node.left)
+                
                 if node.right:
                     queue.append(node.right)
+                # append the left and right children fo the popped node
+
             if queue:
                 ans.append(queue[-1].val)
+            # append the value of the right most ele in the queue
+            # our queue consists of nodes here
         return ans
+
+        
